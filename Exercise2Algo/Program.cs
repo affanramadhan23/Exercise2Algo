8f219ba4d075c5ba29761b70a728aa522e832206
affanramadhan23 <affannabilramadhan@gmail.com>
2022-11-04 16:16:58 +0700

    pembuatan class program

--- Exercise2Algo/Program.cs
+++ Exercise2Algo/Program.cs
@@ -3,3 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class program
+    {
+        private int[] a = new int[91];
+        private int n;
+        public void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Masukkan Jumlah Element Array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 91)
+                    break;
+                else
+                    Console.WriteLine("\nArray maksimum 91 element.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Masukkan Element Array");
+            Console.WriteLine("----------------------");
+
+
+        }
+    }
+}
