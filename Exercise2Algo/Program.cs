6ab3f00786b4c1800ee8da29b1f207fafc507aad
affanramadhan23 <affannabilramadhan@gmail.com>
2022-11-04 16:35:03 +0700

    penambahan metode insertion sort

--- Exercise2Algo/Program.cs
+++ Exercise2Algo/Program.cs
@@ -27,6 +27,54 @@ namespace tester
                 Console.ReadLine();
                 InsertionSort();
             }
+            /* Deklarasi array untuk menampung angka-angka yang akan diurutkan dan elemen array
+            berdasarkan input user (variabel Elements) */
+            int[] Angka = new int[Elements];
+            Console.WriteLine("—————————————————-");
+            /* Metode untuk input angka yang akan disimpan pada masing-masing element di Array */
+            for (int i = 0; i < Elements; i++)
+            {
+                Console.WriteLine("Masukkan angka untuk mengisi elemen " + i + ":");
+                string Input_Temp = Console.ReadLine();
+                int angka;
+                if (int.TryParse(Input_Temp, out angka))
+                {
+                    Angka[i] = Convert.ToInt32(angka);
+                }
+                else
+                {
+                    Console.WriteLine("Maaf input yang Anda masukkan salah.Silahkan tekan Enter untuk mengulang");
+                    Console.ReadLine();
+                    InsertionSort();
+                }
+            }
+            /* Metode Insertion Sort */
+            for (int i = 1; i < Elements; i++)
+            {
+                while (i > 0)
+                {
+                    if (Angka[i - 1] > Angka[i])
+                    {
+                        int Temp = Angka[i - 1];
+                        Angka[i - 1] = Angka[i];
+                        Angka[i] = Temp;
+                        
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            /* Menampilkan hasil pengurutan */
+            Console.WriteLine("");
+            Console.WriteLine("Hasil pengurutan nilai: ");
+            for (int i = 0; i < Elements; i++)
+            {
+                Console.WriteLine("Elemen" + i + ":");
+                Console.WriteLine(Angka[i]);
+            }
         }
     }
 }
